9f2566cc9999fef1e2e89d04c1d2fa590b5ba905
Ramón Briceño <contacto@talentiave.com>
2025-05-16 10:44:38 -0400

    Login and register functional

--- adam/app/register/page.tsx
+++ adam/app/register/page.tsx
@@ -1,18 +1,34 @@
-import { GalleryVerticalEnd } from "lucide-react"
-
+import { LoginForm } from "@/components/login-form"
 import { RegisterForm } from "@/components/register-form"
+import Image from "next/image"
 
-export default function RegisterPage() {
+export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <Image
+              src="/img/logos/logov.png"
+              alt="Logo"
+              width={120}
+              height={120}
+              priority
+            />
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <RegisterForm />
           </div>
-          Acme Inc.
-        </a>
-        <RegisterForm />
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/img/authbackground.jpg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
       </div>
     </div>
   )
